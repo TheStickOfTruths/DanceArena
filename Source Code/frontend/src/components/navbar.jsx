47f1009fc3f4e1fc6271fc62eacda4e7b3ab2a2a
Karlo Visnjovski <karlo.visnjovski@fer.unizg.hr>
2025-11-07 19:51:58 +0100

    Malo promijenio navbar

--- Source Code/frontend/src/components/navbar.jsx
+++ Source Code/frontend/src/components/navbar.jsx
@@ -7,13 +7,8 @@ function Navbar({ setPage }) {
                 <img src="/pictures/logo.png" alt="Dance Arena Logo" className="navbar-logo" />
                 <img src="/pictures/tekst.png" alt="Dance Arena Title" className="navbar-text" />
             </div>
-            <div className="nav-links">
-                <p className="nav-link">Home</p>
-                <p className="nav-link">About</p>
-                <p className="nav-link">Contact</p>
-            </div>
             <div className="account-section">
-                <img src="/pictures/user icon.png" alt="User Icon" className="user-icon" />
+                <img src="/pictures/profile-icon.webp" alt="User Icon" className="user-icon" />
             </div>
 
         </div>
