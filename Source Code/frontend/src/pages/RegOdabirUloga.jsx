7154a9bd33a3f9d11e6855b52db941c56810f75f
Karlo Visnjovski <karlo.visnjovski@fer.unizg.hr>
2026-01-13 15:09:03 +0100

    Prilagoden kod reactovoj formi

--- Source Code/frontend/src/pages/RegOdabirUloga.jsx
+++ Source Code/frontend/src/pages/RegOdabirUloga.jsx
@@ -7,6 +7,13 @@ function RegOdabirUloga() {
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
+    const [newUser, setNewUser] = useState({
+        uloga: '',
+        ime: '',
+        klub: '',
+        mjesto: '',
+        telefon: ''
+    });
 
     if (loading) {
         return (
@@ -17,6 +24,40 @@ function RegOdabirUloga() {
             </div>
         );
     }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewUser((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const userData = {
+                uloga: newUser.uloga,
+                ime: newUser.ime,
+                klub: newUser.klub,
+                mjesto: newUser.mjesto,
+                telefon: newUser.telefon
+            };
+
+            // ime 'createNewUser' treba zamijeniti stvarnom funkcijom za registraciju korisnika
+
+            // const response = await createNewUser(userData);
+            //             if (response) {
+            //                 console.log("Registracija uspješna:", response);
+            //                 navigate('/homepage');
+            //             }
+
+        } catch (error) {
+            console.error("Greška tijekom registracije:", error);
+        }
+    };
+
 
     function oForm(){
         var roleO = document.querySelector('.role-card-o');
@@ -37,6 +78,8 @@ function RegOdabirUloga() {
         imeKlub.style.display = 'none';
         mjestoKlub.style.display = 'none';
         telefon.style.display = 'none';
+
+        newUser.uloga = 'organizator';
     }
 
     function vForm(){
@@ -59,6 +102,8 @@ function RegOdabirUloga() {
         mjestoKlub.style.display = 'block';
         telefon.style.display = 'block';
 
+        newUser.uloga = 'voditelj';
+
     }
 
     function sForm(){
@@ -80,6 +125,8 @@ function RegOdabirUloga() {
         imeKlub.style.display = 'none';
         mjestoKlub.style.display = 'none';
         telefon.style.display = 'none';
+
+        newUser.uloga = 'sudac';
     }
 
     return (
@@ -93,22 +140,50 @@ function RegOdabirUloga() {
                     <div className='role-card-s' onClick={sForm}>Sudac</div>
                 </div>
 
-                <form className='registration-form'>
+                <form className='registration-form' onSubmit={handleSubmit}>
                     <div className='ime'>
-                        <label for='ime'>Ime i prezime: </label>
-                        <input type='text' id='ime' placeholder='Unesite vaše ime i prezime'></input>
+                        <label htmlFor='ime'>Ime i prezime: </label>
+                        <input 
+                            type='text' 
+                            id='ime' 
+                            placeholder='Unesite vaše ime i prezime'
+                            value={newUser.ime}
+                            onChange={handleChange}
+                            required   
+                        />
                     </div>
                     <div className='ime-klub'>
-                        <label for='ime-klub'>Naziv plesnog kluba: </label>
-                        <input type='text' id='ime-klub' placeholder='Unesite naziv plesnog kluba'></input>
+                        <label htmlFor='ime-klub'>Naziv plesnog kluba: </label>
+                        <input 
+                            type='text' 
+                            id='ime-klub' 
+                            placeholder='Unesite naziv plesnog kluba'
+                            value={newUser.klub}
+                            onChange={handleChange} 
+                            required   
+                        />
                     </div>
                     <div className='mjesto-klub'>
-                        <label for='mjesto-kluba'>Mjesto djelovanja kluba </label>
-                        <input type='text' id='mjesto-kluba' placeholder='Unesite mjesto djelovanja kluba'></input>
+                        <label htmlFor='mjesto-kluba'>Mjesto djelovanja kluba </label>
+                        <input 
+                            type='text' 
+                            id='mjesto-kluba' 
+                            placeholder='Unesite mjesto djelovanja kluba'
+                            value={newUser.mjesto}
+                            onChange={handleChange}    
+                            required
+                        />
                     </div>
                     <div className='telefon'>
-                        <label for='telefon'>Broj telefona: </label>
-                        <input type='text' id='telefon' placeholder='Unesite broj telefona'></input>
+                        <label htmlFor='telefon'>Broj telefona: </label>
+                        <input 
+                            type='text' 
+                            id='telefon' 
+                            placeholder='Unesite broj telefona'
+                            value={newUser.telefon}
+                            onChange={handleChange}    
+                            required
+                        />
                     </div>
                     
                     
