426081fb4e79c5a30ae640b306b632fec8ac2e74
RokoCacija <rc55780@fer.hr>
2026-01-18 21:01:17 +0100

    pogresno ime funkcije

--- Source Code/backend/competitions/urls.py
+++ Source Code/backend/competitions/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import send_judge_invite 
 
 urlpatterns = [
-    path('filtered/', views.competition_published, name='competition_published'),
+    path('filtered/', views.competition_filtered, name='competition_published'),
     path('my_competitions/', views.my_competitions, name='my_competitions'),
 
     path('new/', views.competition_create, name='competition_create'),
