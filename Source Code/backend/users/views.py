48d2dfde85ca72dd110b17944755e748629189f9
RokoCacija <rc55780@fer.hr>
2026-01-18 22:03:16 +0100

    .

--- Source Code/backend/users/views.py
+++ Source Code/backend/users/views.py
@@ -87,7 +87,7 @@ def current_user(request):
 
 @api_view(['PUT'])
 @permission_classes([AllowAny]) 
-def put_user_info(request):
+def user_info(request):
     try:
         data = json.loads(request.body)
     except json.JSONDecodeError:
