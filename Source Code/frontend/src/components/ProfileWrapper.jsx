e952a8432b9631e489e7bfbe8d55e50a5260065e
Šimun Vrsalović <simun.vrsalovic@gmail.com>
2026-01-19 22:55:39 +0100

    Security
    - u slučaju da novi korisnik pokuša preskočiti registraciju ne može pristupiti nigdje već ga se automatski vraća nazad na registraciju

--- Source Code/frontend/src/components/ProfileWrapper.jsx
+++ Source Code/frontend/src/components/ProfileWrapper.jsx
@@ -24,7 +24,7 @@ const ProfileWrapper = () => {
             return <ProfileSudac />;
 
         default:
-            return <div>Nepoznata uloga korisnika: {user.role}</div>;
+            return <Navigate to="/registracija" replace />;
     }
 };
 
